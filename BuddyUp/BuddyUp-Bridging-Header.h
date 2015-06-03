2f354e73c4840f966580c917844d1b6405047b15
Yung Dai <yungchidai@gmail.com>
2015-06-03 11:08:04 -0400

    synch

---
+++ BuddyUp/BuddyUp-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+// Parse API
+//#import <Parse/Parse.h>
+//#import <Bolts/Bolts.h>
+//#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+
+
+// Facebook API
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
+//#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+// sliding menu API
+#import "SWRevealViewController.h"
