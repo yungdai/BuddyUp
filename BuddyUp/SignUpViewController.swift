6785aefb0445e27a3168716def224166ce9c31ad
Yung Dai <yungchidai@gmail.com>
2015-06-07 15:52:38 -0400

    finishing signup logic

--- BuddyUp/SignUpViewController.swift
+++ BuddyUp/SignUpViewController.swift
@@ -9,6 +9,96 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordConfirmationField: UITextField!
+    @IBOutlet weak var emailAddressField: UITextField!
+
+    @IBAction func createAccountButtonPressed(sender: UIButton) {
+        
+        // setup the fields for the sign up page
+        let username = usernameField.text
+        let password = passwordField.text
+        let passwordConfirmation = passwordConfirmationField.text
+        let emailAddress = emailAddressField.text
+        
+        var errorText = "Please "
+        let usernameBlankText = "enter a username"
+        let passwordBlankText = "enter a password"
+        let emailBlankText = "eat an email address"
+        let jointText = ", and "
+        let passwordMismatchText = "enter the same password twice"
+        
+        var textError = false
+        
+        
+        // Messaging nil will return 0, so these checks implicitly check for nil text.
+        if count(username) == 0 || count(password) == 0 || count(passwordConfirmation) == 0 {
+            textError = true
+            
+            // Set up the keyboard for the first field missing input:
+            if count(username) == 0 {
+                usernameField.becomeFirstResponder()
+            }
+            
+            if count(password) == 0 {
+                passwordField.becomeFirstResponder()
+            }
+            
+            if count(passwordConfirmation) == 0 {
+                passwordConfirmationField.becomeFirstResponder()
+            }
+
+            if count(emailAddress) == 0 {
+                emailAddressField.becomeFirstResponder()
+            }
+            
+            // error text feedback
+            if count(username) == 0 {
+                errorText += usernameBlankText
+            }
+            
+            if count(password) == 0 || count(passwordConfirmation) == 0 {
+                if count(username) == 0 {
+                    // we need some joining text in the error
+                    errorText += jointText
+                }
+                errorText += passwordBlankText
+            }
+            
+            if count(emailAddress) == 0 {
+                errorText += emailBlankText
+            }
+            
+        } else if password != passwordConfirmation {
+            textError = true
+            errorText += passwordMismatchText
+            passwordField.becomeFirstResponder()
+        }
+        
+        // present a popup if there was an error
+        if textError {
+            let alert = UIAlertController(title: "Alert", message: errorText, preferredStyle: UIAlertControllerStyle.Alert)
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // if those conditions clear you will create a new user and log in
+        
+        var parseUser = PFUser()
+        parseUser.username = usernameField.text
+        parseUser.password = passwordField.text
+        parseUser.signUpInBackgroundWithBlock { (succeeded: Bool, error: NSError?) -> Void in
+            
+            if error == nil {
+                println("signed up user to Parse")
+            } else {
+                println(error)
+            }
+        }
+    
+   
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +109,12 @@ class SignUpViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func processFieldEntries() {
+    
+        
+        
     }
     
 
