30881f112ab2c87426c8b3278f377253a259e5db
Yung Dai <yungchidai@gmail.com>
2015-06-15 14:47:51 -0400

    need to check up on why the popup doesn’t work

--- BuddyUp/NewActivityViewController.swift
+++ BuddyUp/NewActivityViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class NewActivityViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class NewActivityViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
-
+    var startDatePickerPopUp : DatePickerPopUp?
+    var endDatePickerPopUp : DatePickerPopUp?
+    
+    
     @IBOutlet weak var activityImage: UIImageView!
     @IBOutlet weak var activityPicker: UIPickerView!
-    @IBOutlet weak var startTimePicker: UIDatePicker!
-    @IBOutlet weak var endTimePicker: UIDatePicker!
+    
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var endTimeTextField: UITextField!
+    
     
     var selectedActivity = String()
 
@@ -29,18 +34,70 @@ class NewActivityViewController: UIViewController, UIPickerViewDataSource, UIPic
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startDatePickerPopUp = DatePickerPopUp(forTextField: startTimeTextField)
+        endDatePickerPopUp = DatePickerPopUp(forTextField: endTimeTextField)
         activityPicker.dataSource = self
         activityPicker.delegate = self
         selectedActivity = activities[0]
         // Do any additional setup after loading the view.
     }
     
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        if (textField == startTimeTextField) {
+            // make sure that the keyboard doesn't show up when you tap the text field entry box
+            resign()
+            
+            // format the date
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+            dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            let initDate : NSDate? = dateFormatter.dateFromString(startTimeTextField.text)
+            
+            // execute the callback
+            let dataChangeCallBack : DatePickerPopUp.DatePickerPopUpCallBack = { (newDate : NSDate, forTextField : UITextField) ->() in forTextField.text = (newDate.ToDateMediumString() ?? "?") as String
+            }
+            
+            startDatePickerPopUp!.pick(self, initDate: initDate, dataChanged: dataChangeCallBack)
+            return false
+            
+        } else if (textField == endTimeTextField) {
+            // make sure that the keyboard doesn't show up when you tap the text field entry box
+            resign()
+            
+            // format the date
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+            dateFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            let initDate : NSDate? = dateFormatter.dateFromString(endTimeTextField.text)
+            
+            // execute the callback
+            let dataChangeCallBack : DatePickerPopUp.DatePickerPopUpCallBack = { (newDate : NSDate, forTextField : UITextField) ->() in forTextField.text = (newDate.ToDateMediumString() ?? "?") as String
+            }
+            
+            endDatePickerPopUp!.pick(self, initDate: initDate, dataChanged: dataChangeCallBack)
+            return false
+            
+        } else {
+            return true
+        }
+        
+            
+            
+    }
+    
+    
+    func resign() {
+        startTimeTextField.resignFirstResponder()
+        endTimeTextField.resignFirstResponder()
+    }
+    
     @IBAction func saveButtonPressed(sender: UIBarButtonItem) {
         var activity = PFObject(className: "Activity")
         activity["creator"] = PFUser.currentUser()?.username
         activity["name"] = PFUser.currentUser()?.objectForKey("name")
-        activity["startTime"] = startTimePicker.date
-        activity["endTime"] = endTimePicker.date
+        activity["startTime"] = startTimeTextField.text
+        activity["endTime"] = endTimeTextField.text
         activity["activityType"] = selectedActivity
         
         activity.save()
