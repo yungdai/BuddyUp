aa9b398cfc82d70607e58062b15ba12d53ca11c7
Yung Dai <yungchidai@gmail.com>
2015-06-19 15:42:33 -0400

    synch for iMac

--- BuddyUp/MainAppViewController.swift
+++ BuddyUp/MainAppViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class MainAppViewController: UIViewController {
     
     let currentUser = PFUser.currentUser()
+    let iPhoneImageDimension:CGFloat = 100.0
+    let iPadImageDimension:CGFloat = 145.0
     
-
+    @IBOutlet var activityImageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var activityImageWidthConstraint: NSLayoutConstraint!
+    @IBOutlet var personImageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var personImageWidthConstraint: NSLayoutConstraint!
+    
+    
     @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureConstraints()
         let query = PFUser.query()
         query?.findObjectsInBackgroundWithBlock({ (results:[AnyObject]?, error:NSError?) -> Void in
             if let resultsUser = results as? [PFUser],
@@ -38,6 +46,24 @@ class MainAppViewController: UIViewController {
 
     }
 
+    
+    
+    func configureConstraints() {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            activityImageHeightConstraint.constant = iPhoneImageDimension
+            activityImageWidthConstraint.constant = iPhoneImageDimension
+            personImageHeightConstraint.constant = iPhoneImageDimension
+            personImageWidthConstraint.constant = iPhoneImageDimension
+        } else if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            activityImageHeightConstraint.constant = iPadImageDimension
+            activityImageWidthConstraint.constant = iPadImageDimension
+            personImageHeightConstraint.constant = iPadImageDimension
+            personImageWidthConstraint.constant = iPadImageDimension
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
