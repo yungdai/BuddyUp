c61a0eb93fcc1e11a6c11fb63dd0d8ac0f73aea7
Yung Dai <yungchidai@gmail.com>
2015-07-14 12:10:51 -0400

    code update

---
+++ BuddyUp/PictureImageView.swift
@@ -0,0 +1,58 @@
+//
+//  PersonImageView.swift
+//  BuddyUp
+//
+//  Created by Yung Dai on 2015-06-21.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class PictureImageView: UIImageView {
+    
+
+    @IBInspectable var cornerRounding: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRounding
+            
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 1 {
+        didSet {
+            layer.borderWidth = borderWidth
+
+        }
+    }
+    
+    @IBInspectable var customImage: UIImage? {
+        didSet {
+            layer.contents = customImage?.CGImage
+        }
+    }
+ 
+    @IBInspectable var borderColour: UIColor? {
+        didSet {
+            layer.borderColor = borderColour?.CGColor
+        }
+    }
+    
+    
+    override func prepareForInterfaceBuilder() {
+        personImageStyle()
+    }
+    
+    override func awakeFromNib() {
+        personImageStyle()
+    }
+    
+     func personImageStyle() {
+        layer.cornerRadius = cornerRounding
+        layer.borderWidth = borderWidth
+        layer.borderColor = borderColour?.CGColor
+        layer.contents = customImage!.CGImage
+        // binds the masking to the variables above.  This will be true for all images assigned in IB
+        layer.masksToBounds = true
+    }
+
+}
