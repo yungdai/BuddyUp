57e9e7f8599c554df4427b8893e64ce8662937cc
Yung Dai <yungchidai@gmail.com>
2015-07-21 16:07:31 -0400

    search bar is now working

--- BuddyUp/SearchTableViewController.swift
+++ BuddyUp/SearchTableViewController.swift
@@ -8,43 +8,77 @@
 
 import UIKit
 
-class SearchTableViewController: PFQueryTableViewController, UISearchBarDelegate {
+class SearchTableViewController: UITableViewController, UISearchBarDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
     var searchActive: Bool = false
-    var data: [PFObject]!
+    var data: [PFObject] = []
     var filtered: [PFObject]!
-    var userArray: [String] = []
+    var userArray: [PFObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        searchBar.delegate = self
+    
+        search()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func search(searchText: String? = nil){
+        // query the User object
+        let query = PFUser.query()
+        
+        // search the name colum for the text containing the string
+        if(searchText != nil){
+            query!.whereKey("name", containsString: searchText)
+        }
+        
+        query!.findObjectsInBackgroundWithBlock { (results, error) -> Void in
+            self.data = (results as? [PFObject])!
+            print("found something")
+            // if you find the data the reload the screen
+            self.tableView.reloadData()
+        }
+        
     }
-
+    
+    // Define the query that will provide the data for the table view
+    
+//    override func queryForTable() -> PFQuery {
+//        // Start the query object
+//        var query = PFUser.query()
+//        
+//        // Add a where clause if there is a search criteria
+//        if searchBar.text != "" {
+//            query!.whereKey("name", containsString: searchBar.text.lowercaseString)
+//        }
+//        
+//        // Order the results
+//        query!.orderByAscending("name")
+//        
+//        // Return the qwuery object
+//        return query!
+//        
+//    }
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
+        
         return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return the appropriate cells based on how many the search finds
+        return self.data.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+        let obj = self.data[indexPath.row]
+        cell.textLabel!.text = obj["name"] as? String
+        return cell
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -56,24 +90,26 @@ class SearchTableViewController: PFQueryTableViewController, UISearchBarDelegate
     }
     
     func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        // stop searching if you stop typing
+        searchActive = false;
         
-        // Dismiss the keyboard
-        searchBar.resignFirstResponder()
-        
-        // Force reload of table data
-        self.loadObjects()
     }
     
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        //
+        searchActive = false;
         
-        // Dismiss the keyboard
-        searchBar.resignFirstResponder()
-        
-        // Force reload of table data
-        self.loadObjects()
+
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        // as you type you should be able to search at the same time and pull in data with this method
+        search(searchText: searchText)
     }
     
     func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        // stop searching if you click the cancel button
+        searchActive = false;
         
         // Clear any search criteria
         searchBar.text = ""
@@ -82,83 +118,13 @@ class SearchTableViewController: PFQueryTableViewController, UISearchBarDelegate
         searchBar.resignFirstResponder()
         
         // Force reload of table data
-        self.loadObjects()
+        self.tableView.reloadData()
     }
     
-    // Define the query that will provide the data for the table view
     
-    override func queryForTable() -> PFQuery {
-        // Start the query object
-        var query = PFUser.query()
-        
-        // Add a where clause if there is a search criteria
-        if searchBar.text != "" {
-            query!.whereKey("searchText", containsString: searchBar.text.lowercaseString)
-        }
-        
-        // Order the results
-        query!.orderByAscending("username")
-        
-        // Return the qwuery object
-        return query!
-
-    }
-
     
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
-        return cell
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
